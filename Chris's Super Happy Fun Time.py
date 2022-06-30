cb155e13ef6d609fa5e8358ed5809fc7fd43c65a
DatSoup <campbell.chrismn@gmail.com>
2022-06-29 22:45:06 -0500

    Made numerous edits to the story

--- Chris's Super Happy Fun Time.py
+++ Chris's Super Happy Fun Time.py
@@ -193,7 +193,7 @@ def main():
 #Dragonkin/tinker story
         if (Race, Class) == ('dragonkin', 'hextinker'):
                 print("\nDeep within the heart of ocean exists an island shrouded in ominous fog. Snow peaked mountain ranges stretching for miles is all that can be seen."
-                      "\ndDeep within the mountains exists the capital known as Dragon's Nest. Here a young dragonkin HexTinker is hard at work within the HexTinker workshop."
+                      "\nWoven far within these mountains exists the capital known as Dragon's Nest. Here a young dragonkin HexTinker is hard at work within his workshop."
                       "\n\n\"You there. what is your name?\" A rough voice bellows")
 #Player name input and setting inventory
                 inventory = []
@@ -209,14 +209,14 @@ def main():
                                 print("\n\"Good. You have made the correct decision in learning of our heritage young one.\""
                                       "\n\"Only in knowing your true identity will your purpose begin to reveal itself...\"")
                                 print("\n\"Long ago. In the ancient days of our ancestrals dragons, us dragonkins did not yet exist.\""
-                                      "\n\"During the age of dragons. Few dared to enter these mountains. Our ancestors were mercilous, beasts. Always looking to protect the mark of the gods.\""
+                                      "\n\"During the age of dragons, few dared to enter these mountains. Our ancestors were mercilous beasts. Always looking to protect the mark of the gods.\""
                                       "\n\"However, the allied races grew bold. The humans, orcs, and elves all came together under one banner to sail to this island and enter the mountains in search of resources.\""
-                                      "\n\"Thus was the beginning of the great war. Dragons echoing bellows instilled fear into the hearts of their enemies. Even united under the banner of the allied races, fear coursed through their veins.\""
+                                      "\n\"Thus was the beginning of the great war. Dragons echoing bellows instilled fear into the hearts of their enemies. Even united under the banner of the allied races, fear coarsed through their veins.\""
                                       "\n\"For months, the dragons fought viciously against the allied races. While fewer in number, the dragons were resilient. Hundreds of their enemies would die for every single dragon slain in battle.\""
                                       "\n\"It wasn't until the dragons began to notice the intentions of the allied races did the fighting begin to slow. Our ancestors could not communicate with any living beings beside dragons.\""
-                                      "\n\"After many had died, and many grew weary of battle, the dragons decided to take a chance upon allied races."
-                                      "\n\"They decided to allow 1 member of each of the allied races to present themselves before the mark of the gods. Using’s its power, they were able to create the first dragonkin\""
-                                      "\n\"That is why we can communicate with all races within our world, it is our purpose.\""
+                                      "\n\"After many had died and many grew weary of battle, the dragons decided to take a chance upon allied races."
+                                      "\n\"They decided to allow 1 member of each of the allied races to present themselves before the mark of the gods. Using it's power, they were able to create the first dragonkin\""
+                                      "\n\"This is why we can communicate with all races within our world. It is our purpose.\""
                                       "\n\"It is why we as dragonkin are here today as servants to the communications of this world. More important even then HexCr-…\"")
                                 break
             
@@ -268,25 +268,25 @@ def main():
 #Post combat 1            
                 print("\nYour enemy has been deafeated.")
                 print("\nAs you approach the corpse you begin to notice this no creature you have seen before. It's body looked like bones held together by tough muscular sineu."
-                      "Like some sort of demon.")
+                      " Like some sort of demon.")
                 print("\n\"Drak'Shir\" rumbled BarShoup")
-                print("\n\"They are a race who live in caverns deep within the earth. Cavernous holes oozing with hot lava is where these monsters call home.\"")
+                print("\n\"They are a race who live in caverns deep within the earth. Cavernous holes oozing with hot lava is what these monsters call home.\"")
                 print("\n\"I thought they were all purged from this land during the great war. But I'll spare you the stories for now.")
                 print("\n\"Be on your guard " + playerName + '. Lets go.\"')
                 print("\n***Upon rummaging through the corpse, you found a strange medalion. Perhaps it holds some value")
                 inventory.append('Drak\'Shir Medallion')
                 print("Inventory: ", inventory)
-                print("\n While running towards the mark of the gods the feeling of death and chaos fill the space around you. The images of flames dancing off the bronze structures illuminate the capital.")
+                print("\n While running towards the mark of the gods, the feeling of death and chaos fill the space around you. The images of flames dancing off the bronze structures illuminate the capital.")
                 print("\n Wailing screams echo amongst the roaring of dragons. From the corner of you eye the great draconic behemoths rage through the city decimating Drak'Shir in waves of fire.")
                 print("\n\"What is this madness? There are so many of them.\" BarShoup speaks with grimmace")
                 print("\n A menacing cackle begins to permeate from the battlefield. You notice A flaming demonic figure of bone and torn flesh growing bigger within the dragons fire. As if it simply asorbed the flames of the great dragons.")
                 print("\n\"Furnace Fiends.\" BarShoup spoke gravely")
-                print("\n\"This is bad. Those monsters may be the greatest threat to dragon kind. " + playerName + " Hurry! We must protect the mark of the gods!")
-                print("\nWinding through the corridors echoes grow and fade. The sounds of battles flowing in waves. Then suddly appeares the great bronze doorway, the entryway of the mark.")
+                print("\n\"This is bad. Those monsters may be the greatest threat to dragon kind. " + playerName + ", hurry! We must protect the mark of the gods!")
+                print("\nWinding through the corridors, echoes grow and fade. The sounds of battles flowing in waves. Then suddly appeares the great bronze doorway. The entryway of the mark.")
                 print("\n\"Where are the guards? DragonsBlight!\" Cursed BarShoup.")
-                print("\nA furnace fiend ghoulish laugh rippled throughtout the air. Charred bones fell from his gaping maw.")
-                print("\n\"The great ones should be protecting the mark through the door, quick " + playerName + " You most go to ensure the guardians still protect the mark! I'll be there soon, leave this pestilent creature to me.")
-                print("\nHeading Barshoup word, you run towards the doors while you hear a Barshoup emminate a ferocious roar as he engaged the enemy")
+                print("\nA furnace fiend\'s ghoulish laugh rippled throughout the air. Charred bones fell from his gaping maw.")
+                print("\n\"The great ones should be protecting the mark through the door. Quick, " + playerName + ". You must go to ensure the guardians still protect the mark! I'll be there soon, leave this pestilent creature to me.")
+                print("\nHeading Barshoup\'s word, you run towards the doors while you hear Barshoup emminate a ferocious roar as he engaged the enemy")
                 print("\nSuddenly, while approaching the door a drak'shir mercenary flanks you!")
                 
 #Second combat phase 
@@ -327,14 +327,14 @@ def main():
                                 print("Forces beyond you understanding have chosen to give you life. playerHP = " + playerHP)
                 
                 print("////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////")
-                print("\nUpon defeating the enemy you turn towards the supernatural doors to the great mark.")
+                print("\nUpon defeating the enemy, you turn towards the supernatural doors to the great mark.")
                 print("\nSwirling with a potent energy, you hesitate as you approach the door.")
                 print("\nMustering the courage you slowly push open the bronze doors to the halls of the Great mark.")
                 print("\nAs you enter, the room swirls with cosmic energy. Constellations dance as reflections upon the jagged rocks filling the room.")
                 print("\nWithin the center of the room, 3 ethereal dragons loom with their eyes closed.")
-                print("\nEven though their eyes are closed, you can feel them staring at you. As if their own consciousness was some how pressing upon you mind.")
+                print("\nEven though their eyes are closed, you can feel them staring at you. As if their own consciousness was some how pressing upon your mind.")
                 print("\nBetween them, you see flickers of a glowing stone hidden through their shifting spirit like forms.")
-                print("\n\"...Greetings, little dragon...\" Whispered a hoarse femenin voice")
+                print("\n\"...Greetings, little dragon...\" Whispered a hoarse feminine voice")
                 print("\n\"I am Moth'reza, mother of the dragons. Your heart must be pure to enter within those doors.\" She explained solemnly.")
                 print("\n\"We feel the looming darkness crashing at the gates, perversly fighting to get to this rune by force.\" She mused.")
                 print("\n\"Our power can hold the room for now, but I fear in time the enemy will penetrate our barrier. We can sense the growing evil of the forces waging against us.\"")
@@ -345,7 +345,7 @@ def main():
                 if answer2.upper() == 'N':
                         print("\n\"Unfortunately little dragon, you are our only hope. You must go.")
                 elif answer2.upper() == 'Y':
-                        print("\n\"Thank you little dragon. We then bestow upon you our blessing before we send you on your way.\"")
+                        print("\n\"Thank you, little dragon. We now bestow upon you our blessing before we send you on your way.\"")
                         print("\n***The celestial dragons have bestowed upon you their blessings. Your health has been restored and abilities have been enhanced.")
                         for key, val in playerSkills.items():
                                 playerSkills[key] = val + 10
@@ -353,21 +353,23 @@ def main():
                         print("The celestial dragons grow inpatient of your indecisiveness. Try again.")
                 
                 print("\nSuddenly, the door opens again and you see BarShoup hurry in your direction.")
-                print("\n\"Ah, BarShoup. It is good to see you again.\" Moth'reza spoke with a hint of excitment")
+                print("\n\"Ah, BarShoup. It is good to see you again.\" Moth'reza spoke with a hint of excitment.")
                 print("\n\"It is an honor to be in your presence great ones.\" BarShoup exclaimed while kneeling before the celestial dragons.")
                 print("\n\"Rise my child, time runs short. We will be sending you and " + playerName + " to the dragon cliff shores. Intercept the hidden lotus and go to the allied races for help.")
-                print("\n\"Yes great mother.\" BarShoup spoke with a hint of pride.")
-                print("\n\"Good, we will send you both now. Ah and before we forget. " + playerName + " you notice the ability to sense the cosmic veins around you now. This is apart of our blessing. Tap into this resource if you are ever in need of healing.\"")
+                print("\n\"Yes, Great Mother.\" BarShoup spoke with pride.")
+                print("\n\"Good, we will send you both now. Ah, and before we forget, " + playerName + ", you will notice the ability to sense the cosmic veins around you now. This is a part of our blessing. Tap into this resource if you are ever in need of healing.\"")
                 playerSkills.update({'CV' : 20})
                 print("***You can now use the ability Cosmic Vein (CV) to enter a healing interaction while in combat")
                 print("\nThe ground begins to swirl beneath you as a radiant cloud begins to slowly enshroud you in mist. Dancing cosmic energy sings with an unsettled song. As if it senses the the dark forces trying to subdue it.")
-                print("\Within moments, the mist begins to fade and you stand at the edge of a massive cliff. Waves crashing far below you.")
-                print("\n\"We will go now.\" BarShoup spoke swiftly")
-                print("Then within moments you look onwards are BarShoup transforms into a massive greathawk.")
-                print("\nGrabbing you with his talons BarShoup soares into the air flying into the dense mist covering the sea.")
-                print("\n" + playerName + " We will now be heading to the hidden lotus. Our hidden jewel which protects our lands from outsider. I was once a scout for this ship. Prepare to prove yourself to the crew before they allow you to sail upon this vessel of legends.")
+                print("Within moments, the mist begins to fade and you stand at the edge of a massive cliff. Waves crashing far below you.")
+                print("\n\"We must go now.\" BarShoup speaks swiftly")
+                print("\nLooking onwards, you notice that within moments BarShoup transforms into a massive greathawk.")
+                print("\nGrabbing you with his talons, BarShoup soares into the air flying into the dense mist covering the sea.")
+                print("\n" + playerName + ", we will now be heading to the hidden lotus. It is our jewel which protects our lands from outsider. I was once a scout for this ship. Prepare to prove yourself to the crew before they allow you to sail upon this vessel of legends.")
                 print("/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////")
-                print("\n")                
+                print("\n")   
+                
+                input("to be continued...")
                         
         
 
